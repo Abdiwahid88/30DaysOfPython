15bc13572c21d755ae91818d1eb3886b063edb63
abdiwahid <abdiwahid.abdu@gmail.com>
2023-01-27 13:30:33 +0000

    this is new commit first day of learning python

---
+++ day1.py
@@ -0,0 +1,41 @@
+"""
+This my first lesson 
+Sting = letters 
+Variable A variable is created the moment you first assign a value to it.
+A variable must start letter or _ and can not start hashtag 
+A variable must start alpha numeric A-Z or 0-9
+Variables do not need to be declared with any particular type, and can even change type after they have been set
+There is Global veriable if the veriable has no funcation 
+Data types 
+    1: sring waa xarfo x= abdi 
+
+    2:integer is numbers only 
+    3: float is decemal nothing else 
+    4: list 
+"""
+#print("Hello World!")
+#x, y = 10, "abdi"
+#print(x,y)
+#x = "my name is "
+#y = "abdi"
+#print(x+y)
+
+#x = 10 
+#y = 10
+#print(x+y)
+
+#x = 10 
+#y = 10 
+#print(x,y)
+#y = 10 
+#myString = str(y)
+#print(type(myString)) this is called casting 
+#myNum = 2.5 
+#myNumInt = Int (myNum)
+#print(type(myNumInt))
+
+x = ["moos, hilib, bariis"]
+myItems = (("hilib", "sonkor", "bur"))
+myList = list(myItems)
+print(type(x))
+
